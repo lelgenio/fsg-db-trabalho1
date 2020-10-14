37393df9dd58c02d8203e2fe46d2eae213748f72
lelgenio <lelgenio@disroot.org>
2020-10-14 20:55:19 -0300

    formatar 1

--- ex1.sql
+++ ex1.sql
@@ -1,5 +1,11 @@
-# Exercicio 1
+-- Selecionar todos os EMPREGADOS (primeiro e último nome) juntamente com o nome do gerente, que tenham como gerente a “Karen Partners”, relacionando a tabela empregados com ela mesma.
 
-select * from EMPREGADOS;
+SELECT * FROM EMPREGADOS;
 
-select PRIMEIRO_NOME, ULTIMO_NOME, GERENTE_ID FROM EMPREGADOS WHERE GERENTE_ID = 146
+SELECT
+    PRIMEIRO_NOME, ULTIMO_NOME, GERENTE_ID
+FROM
+    EMPREGADOS
+WHERE
+    GERENTE_ID = 146
+    ;
