1e66b319262af0e9912f924bd78266b2cdcd1835
leosoaress <72895525+leosoaress@users.noreply.github.com>
2020-10-14 20:23:36 -0300

    Update and rename ex6 to ex6.sql

--- ex6.sql
+++ ex6.sql
@@ -3,5 +3,5 @@
 select * FROM PAISES;
 
 INSERT INTO PAISES values ('PR', 'Paraguai', '2');
-INSERT INTO PAISES values ('CL', 'Chile', '2');
+INSERT INTO PAISES values ('CL', 'Chile', '2');'
 INSERT INTO PAISES values ('VE', 'Venezuela', '2');
