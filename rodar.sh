f3180ac315e6e27e6238e182fa6ccb2d1de7b9cd
lelgenio <lelgenio@disroot.org>
2020-10-14 21:46:14 -0300

    adicionar rodar.sh e resultados

---
+++ rodar.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Parar se der erro
+set -e
+
+CMD="mariadb -h34.95.201.6 -P3306 -ulelgenio -ppuredebatata DB_LEUGENIO"
+
+rodaSQL(){
+    echo ==== $i ====
+    $CMD -v < ex$i.sql | tee ex$i.out
+}
+
+echo ==== carregando dump ====
+$CMD -v < dump.sql
+echo ==== dump carregado  ====
+for i in $(seq 1 10)
+do
+    rodaSQL $i
+done
+
+
+
