db0931b2e7dfb089f00ffbda4b9d4bc6135faa38
lelgenio <lelgenio@disroot.org>
2020-10-14 22:07:30 -0300

    add matricula

--- ex4.sql
+++ ex4.sql
@@ -1,7 +1,7 @@
 -- Selecionar todos os funcionários que estão com o salário fora da faixa da sua função (MIN_SALARIO, MAX_SALARIO), listar o primeiro e último nome, matricula e salario.
 
 SELECT
-    PRIMEIRO_NOME AS 'NOME', ULTIMO_NOME AS 'SOBRENOME', SALARIO
+    PRIMEIRO_NOME AS 'NOME', ULTIMO_NOME AS 'SOBRENOME', SALARIO, EMPREGADO_ID AS 'MATRICULA'
 FROM
     FUNCOES, EMPREGADOS
 WHERE
