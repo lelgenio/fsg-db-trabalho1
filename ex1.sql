97e9ef74ffb37e2d1185b9c31c042772e36cb1d6
leosoaress <72895525+leosoaress@users.noreply.github.com>
2020-10-14 21:25:08 -0300

    Update ex1.sql

--- ex1.sql
+++ ex1.sql
@@ -1,10 +1,11 @@
 -- Selecionar todos os EMPREGADOS (primeiro e último nome) juntamente com o nome do gerente, que tenham como gerente a “Karen Partners”, relacionando a tabela empregados com ela mesma.
 
 SELECT
-    PRIMEIRO_NOME, ULTIMO_NOME, GERENTE_ID
+    empregado.PRIMEIRO_NOME, empregado.ULTIMO_NOME, gerente.PRIMEIRO_NOME AS 'NOME DO GERENTE', gerente.ULTIMO_NOME AS 'SOBRENOME'
 FROM
     EMPREGADOS as empregado,
     EMPREGADOS as gerente
 WHERE
-    GERENTE_ID = 146
-    ;
+    empregado.GERENTE_ID = 146 AND empregado.GERENTE_ID = gerente.EMPREGADO_ID
+    
+;
